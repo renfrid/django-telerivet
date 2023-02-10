fcbdcdeeb305cb1893d894ffa143f81e679e9716
renfrid <ngolongolo@multics.co.tz>
2023-02-10 11:55:04 +0300

    Minor change

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -17,35 +17,7 @@ class DashboardView(View):
         return super(DashboardView, self).dispatch( *args, **kwargs)
 
     def get(self, request):
-        """user creation"""
-        citizens = Citizen.objects.filter(status='VERIFIED',is_active=1)
-
-        for val in citizens:
-            name = val.name
-            unique_id = val.unique_id 
-            password = val.password
-
-            """create user before update citizen"""
-            arr_name = name.split(' ', maxsplit=2)
-            first_name = arr_name[0]
-            last_name = arr_name[1]
-            email = f'{first_name.lower()}@jinadi.co.tz'
-
-            """default role"""
-            role = Group.objects.get(name='citizen')
-
-            """register user"""
-            user = User.objects.create_user(username=unique_id, email=email, password=password)
-            user.first_name = first_name
-            user.last_name = last_name
-            user.is_active = False
-            user.groups.add(role)
-            user.save()
-
-            """update citizen"""
-            Citizen.objects.filter(id=val.id).update(user_id=user.id)
-
-
+        """get citizen"""
         citizen = Citizen.objects.filter(designation="MWANANCHI")
 
         """Filter per status"""
